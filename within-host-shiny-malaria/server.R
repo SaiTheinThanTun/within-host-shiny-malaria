2907e6049ba8626b600d3b6ac269065f028859e4
SaiMaci9 <theinthantun.sai@gmail.com>
2019-01-17 22:59:40 +0700

    working

--- within-host-shiny-malaria/server.R
+++ within-host-shiny-malaria/server.R
@@ -3,22 +3,19 @@ library(shiny)
 #utility functions go here
 source("UtilityFunctions.R", local = TRUE)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
   output$NJWimPlot <- renderPlot({
     plot(NJWIm(input$initn,48,input$mu,input$sig,input$pmf,k0,a,tpar,delay,2400,input$initconc,0.693,input$halflife,
-               input$killrate,input$ce50,input$h),xlim=c(0,245),ylim=c(0,8),type=show)
+               input$killrate,input$ce50,input$h),xlim=c(0,245),ylim=c(0,8), type = 'l')
   })
   
   output$drugfPlot <- renderPlot({
-    plot(drugf(2400,input$initconc,0.693,input$halflife,input$killrate,input$ce50,input$h),xlim=c(0,245),ylim=c(0,80),
-         type=show)
+    plot(drugf(2400,input$initconc,0.693,input$halflife,input$killrate,input$ce50,input$h),xlim=c(0,245),ylim=c(0,80), type = 'l')
   })
   
   output$drugeffPlot <- renderPlot({
-    plot(drugeff(2400,input$initconc,0.693,input$halflife,input$killrate,input$ce50,input$h),xlim=c(0,125),ylim=c(0,100),
-         type=show)
+    plot(drugeff(2400,input$initconc,0.693,input$halflife,input$killrate,input$ce50,input$h),xlim=c(0,125),ylim=c(0,100), type = 'l')
   })
   
 })
