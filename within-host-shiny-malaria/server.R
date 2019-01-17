e1a4306ee6ace9d144effbdd6c8a4907b90e1ba2
SaiMaci9 <theinthantun.sai@gmail.com>
2019-01-17 22:13:51 +0700

    not working version with input fixed

--- within-host-shiny-malaria/server.R
+++ within-host-shiny-malaria/server.R
@@ -5,54 +5,20 @@ source("UtilityFunctions.R", local = TRUE)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
   
   output$NJWimPlot <- renderPlot({
-    plot(NJWIm(initn,48,mu,sig,pmf,k0,a,tpar,delay,2400,initconc,0.693,halflife,killrate,ce50,h),xlim=c(0,245),ylim=c(0,8),type=show),
-    initn=slider(10,1000),
-    mu=slider(1,48),
-    sig=slider(1,48),
-    pmf=slider(1,30),
-    # k0=slider(0,1,step = 0.01),
-    # a=slider(0.0001,0.01,step = 0.001),
-    # tpar=slider(1000,100000,step = 10),
-    # delay=slider(0,4,step=1),
-    initconc=slider(25,150,step=5),
-    halflife=slider(5,240,step=1),
-    killrate=slider(0.001,1,step=0.05),
-    ce50=slider(10,100,step=5),
-    h=slider(1,10,step=1),
-    show=picker("line"="l", "points"="p")
+    plot(NJWIm(input$initn,48,input$mu,input$sig,input$pmf,k0,a,tpar,delay,2400,input$initconc,0.693,input$halflife,
+               input$killrate,input$ce50,input$h),xlim=c(0,245),ylim=c(0,8),type=show)
   })
   
   output$drugfPlot <- renderPlot({
-    plot(drugf(2400,initconc,0.693,halflife,killrate,ce50,h),xlim=c(0,245),ylim=c(0,80),type=show),
-    initconc=slider(25,150,step=5),
-    halflife=slider(5,240,step=1),
-    killrate=slider(0.001,1,step=0.05),
-    ce50=slider(10,100,step=5),
-    h=slider(1,10,step=1),
-    show=picker("line"="l", "points"="p")
+    plot(drugf(2400,input$initconc,0.693,input$halflife,input$killrate,input$ce50,input$h),xlim=c(0,245),ylim=c(0,80),
+         type=show)
   })
   
   output$drugeffPlot <- renderPlot({
-    plot(drugeff(2400,initconc,0.693,halflife,killrate,ce50,h),xlim=c(0,125),ylim=c(0,100),type=show),
-    initconc=slider(25,150,step=5),
-    halflife=slider(5,240,step=1),
-    killrate=slider(1,100,step=0.5),
-    ce50=slider(10,100,step=5),
-    h=slider(1,10,step=1),
-    show=picker("line"="l", "points"="p")
+    plot(drugeff(2400,input$initconc,0.693,input$halflife,input$killrate,input$ce50,input$h),xlim=c(0,125),ylim=c(0,100),
+         type=show)
   })
   
 })
--- within-host-shiny-malaria/ui.R
+++ within-host-shiny-malaria/ui.R
@@ -22,12 +22,37 @@ shinyUI(fluidPage(
                    "Number of bins:",
                    min = 1,
                    max = 50,
-                   value = 30)
+                   value = 30),
+       sliderInput("initn", "Initial total parasites: ", min = 10, max=1000, value=10),
+       sliderInput("mu", "Mean of initial age distribution: ", min = 1, max=48, value = 1),
+       sliderInput("sig", "SD of initial age distribution: ", min = 1, max=48, value = 1),
+       sliderInput("pmf", "Parasite Multiplication factor: ", min = 1, max=30, value=1),
+       sliderInput("initconc", "Initial drug concentration: ", min = 25, max=150, value = 25, step = 5 ),
+       sliderInput("halflife", "Drug halflife: ", min = 5, max=240, value=5, step = 1),
+       sliderInput("killrate", "Kill rate: ", min = .001, max = 1, value = .001, step = .05),
+       sliderInput("ce50", "CE50: ", min = 10, max = 100, value = 10, step = 5),
+       sliderInput("h", "h: ", min = 1, max = 10, value =1, step =1)
     ),
+    
+    ##initn=slider(10,1000),
+    ##mu=slider(1,48),
+    ##sig=slider(1,48),
+    ##pmf=slider(1,30),
+    # k0=slider(0,1,step = 0.01),
+    # a=slider(0.0001,0.01,step = 0.001),
+    # tpar=slider(1000,100000,step = 10),
+    # delay=slider(0,4,step=1),
+    ##initconc=slider(25,150,step=5),
+    ##halflife=slider(5,240,step=1),
+    ##killrate=slider(0.001,1,step=0.05),
+    ##ce50=slider(10,100,step=5),
+    ##h=slider(1,10,step=1),
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+       plotOutput("NJWimPlot"),
+       plotOutput("drugfPlot"),
+       plotOutput("drugeffPlot")
     )
   )
 ))
