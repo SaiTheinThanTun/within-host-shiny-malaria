b3aa1def1eca2cd0cc38e67dfdd72afb4454ed90
SaiMaci9 <theinthantun.sai@gmail.com>
2019-01-17 17:03:32 +0700

    comments in r main file
    - comments in r main file
    - shiny app init
    - drugeff and drugf comparison with filemerge

--- within_host_drug_action.R
+++ within_host_drug_action.R
@@ -1,39 +1,52 @@
 # Ref. White NJ, Chapman D, Watt G (1992) The effects of multiplication and synchronicity 
 # on the vascular distribution of parasites in falciparum malaria. Trans R Soc Trop Med Hyg 86:590-597.
 
-#age distribution of parasites
+#1. age distribution of parasites
 InitAgeDistribution <- function(initN,lifecycle,mu,sigma)
 {
+  #initN: initial number of total parasites (across all stages)
+  #lifecycle: number of hours parasites survive at birth. also serves as the length of storage at each stage
+  #mu and sigma: parameters for standard density distribution 
+  #centered at mu with SD sigma
   distr<-dnorm(1:lifecycle,mu,sigma)
   tot<-sum(distr[1:lifecycle])
   distr*initN/tot  
 }
-# aging
-ShiftOneHour<-function(ls,pmf)
+#2. aging
+ShiftOneHour<-function(lst,pmf)
 {
-  n<-length(ls) #n is 48
-  c(ls[n]*pmf,ls[1:(n-1)])
+  #lst is the density distribution of all parasites at a single timepoint
+  #i.e. initial distribution is created by InitAgeDistribution function
+  #parasites are assumed to live up to 48 hours
+  #after which they multiply by pmf (parasite multiplication factor) and die (therefore shift in the array)
+  n<-length(lst) #n is 48
+  c(lst[n]*pmf,lst[1:(n-1)])
 }
-# for comparing with the observed
+#3. for comparing with the observed
 countrings<-function(lst){
   MaxAge<-26   #maximum age that can be observed
   sum(lst[1:MaxAge]) #only up to hour 26
 }
 
 initconc<-1
+#4. drug concentration
 drugconcentration<-function(t,initconc,drugloss,halflife){
+  #t timestep
+  #initconc: initial concentration
+  #drugloss: rate of drug loss?
+  #halflife: time at which only 50% of original concentration will remain
   initconc*exp(-drugloss*(t/halflife))
 }
 #so far only one dose
 
-
+#5. drug action: killing the parasites depending on the drug concentration and ce50
 drugaction<-function(t,killrate,drugconcentration,ce50,h){
   killrate/(1+(ce50/drugconcentration)^h) #ce50 is the tipping point of the sigmoidal curve, h is curvature of the sigmoidal curve
 }
 #overall killrate would be the sum of 2: artemisinin + partner drug
 
 h<-1
-## NJW model main function + Immunity
+##6. NJW model main function + Immunity
 K<-function(a,t,delay,tpar,parasites,k0){
   k0*(parasites/tpar)+a*(t/(delay))
 }
@@ -41,9 +54,10 @@ K<-function(a,t,delay,tpar,parasites,k0){
 #a innate immune response (lower than the adaptive immuntiy) which is delayed
 #innate and 
 
+#7. simulate parasite age distribution over time after taking a single dose of artemisinin
 NJWIm<-function(initn,lc,mu,sig,pmf,k0,a,tpar,delay,runtime,initconc,drugloss,halflife,killrate,ce50,h){
-  biglst<-matrix(0,nrow=runtime,ncol=lc)
-  druglst<-matrix(0,nrow=runtime,ncol=lc)
+  biglst<-matrix(0,nrow=runtime,ncol=lc) #store parasite age distribution
+  druglst<-matrix(0,nrow=runtime,ncol=lc) #store drug effect or conc? not used in this function!!
 
   lst <- InitAgeDistribution(initn,lc,mu,sig)
   biglst[1,]<-lst
@@ -76,6 +90,7 @@ NJWIm<-function(initn,lc,mu,sig,pmf,k0,a,tpar,delay,runtime,initconc,drugloss,ha
   }
   
   data.frame(time=seq(1,runtime),log10=log10(apply(biglst,1,countrings)))
+  #output the log of total observable parasites
 
 }
 
@@ -100,7 +115,7 @@ manipulate(plot(NJWIm(initn,48,mu,sig,pmf,k0,a,tpar,delay,2400,initconc,0.693,ha
            show=picker("line"="l", "points"="p")
 )
 
-
+#8. drug concentration over time (drug effect on the next function drugeff)
 drugf<-function(runtime,initconc,drugloss,halflife,killrate,ce50,h){
   druglst<-matrix(0,nrow=runtime,ncol=2)
   
@@ -120,7 +135,7 @@ drugf<-function(runtime,initconc,drugloss,halflife,killrate,ce50,h){
     i<-i+1   
   }
   
-  data.frame(time=seq(1,runtime),log10=druglst[,1])
+  data.frame(time=seq(1,runtime),log10=druglst[,1]) #<- only difference is here (column subset:1)
   
 }
 
@@ -134,6 +149,7 @@ manipulate(plot(drugf(2400,initconc,0.693,halflife,killrate,ce50,h),xlim=c(0,245
            show=picker("line"="l", "points"="p")
 )
 
+#9. drug effect over time (drug concentration is on the previous function drugf)
 drugeff<-function(runtime,initconc,drugloss,halflife,killrate,ce50,h){
   druglst<-matrix(0,nrow=runtime,ncol=2)
   
@@ -153,7 +169,7 @@ drugeff<-function(runtime,initconc,drugloss,halflife,killrate,ce50,h){
     i<-i+1   
   }
   
-  data.frame(time=seq(1,runtime),log10=druglst[,2])
+  data.frame(time=seq(1,runtime),log10=druglst[,2]) #<- only difference is here (column subset:2)
   
 }
 manipulate(plot(drugeff(2400,initconc,0.693,halflife,killrate,ce50,h),xlim=c(0,125),ylim=c(0,100),type=show),
