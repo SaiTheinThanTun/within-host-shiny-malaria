c583b9a205788101380f4c7a6ba1fc0707900244
SaiMaci9 <theinthantun.sai@gmail.com>
2019-01-30 12:52:19 +0700

    sensitivity slider
    init n log10 scale
    sensitivity slider

--- within-host-shiny-malaria/server.R
+++ within-host-shiny-malaria/server.R
@@ -5,19 +5,24 @@ source("UtilityFunctions.R", local = TRUE)
 
 shinyServer(function(input, output) {
   
+  initn_R <- reactive(10^input$initn) #initial parasites in 10-power scale
+  killrate_R <- reactive(input$killrate*input$sen) #killrate adjusted by sensitivity
+  killrate_2_R <- reactive(input$killrate_2*input$sen_2)
+  
   output$combinedPlot <- renderPlot({
-    # parasiteDensity <- reactive(NJWIm(input$initn,48,input$mu,input$sig,input$pmf,k0,a,tpar,delay,2400,input$initconc,0.693,input$halflife,
+    # parasiteDensity <- reactive(NJWIm(initn_R(),48,input$mu,input$sig,input$pmf,k0,a,tpar,delay,2400,input$initconc,0.693,input$halflife,
     #                                   input$killrate,input$ce50,input$h))
-    parasiteDensity_2D <- reactive(NJWIm_2(input$initn,48,input$mu,input$sig,input$pmf,k0,a,tpar,delay,2400,input$initconc,0.693,input$halflife,
-                                      input$killrate,input$ce50,input$h, input$initconc_2,0.693,input$halflife_2,
-                                      input$killrate_2,input$ce50_2))
+    parasiteDensity_2D <- reactive(NJWIm_2(initn_R(),48,input$mu,input$sig,input$pmf,k0,a,tpar,delay,2400,input$initconc,0.693,input$halflife,
+                                      killrate_R(),input$ce50,input$h, input$initconc_2,0.693,input$halflife_2,
+                                      killrate_2_R(),input$ce50_2))
     
-    drugConc_A <- reactive(drugf(2400,input$initconc,0.693,input$halflife,input$killrate,input$ce50,input$h))
-    drugConc_B <- reactive(drugf(2400,input$initconc_2,0.693,input$halflife_2,input$killrate_2,input$ce50_2,input$h))
+    drugConc_A <- reactive(drugf(2400,input$initconc,0.693,input$halflife,killrate_R(),input$ce50,input$h))
+    drugConc_B <- reactive(drugf(2400,input$initconc_2,0.693,input$halflife_2,killrate_2_R(),input$ce50_2,input$h))
     
     par(mar=c(5, 4, 4, 6) + 0.1)
     # plot(parasiteDensity(), axes=FALSE, xlab="", ylab="",xlim=c(0,245),ylim=c(0,8), type = 'l')
-    plot(parasiteDensity_2D(), axes=FALSE, xlab="", ylab="",xlim=c(0,245),ylim=c(0,8), type = 'l')
+    plot(parasiteDensity_2D(), axes=FALSE, xlab="", ylab="",xlim=c(0,245),ylim=c(0,10), type = 'l')
+    text(x = 120, y=7, paste("Sum of observable parasites: ", sum(round(parasiteDensity_2D()[,2]))))
     axis(2, col="black", col.axis="black", las=1)
     mtext("Parasite density (log10)", side=2, col="black", line=3)
     #box()
@@ -31,11 +36,11 @@ shinyServer(function(input, output) {
   })
   
   output$drugeffPlot <- renderPlot({
-    #plot(drugeff(2400,input$initconc,0.693,input$halflife,input$killrate,input$ce50,input$h),xlim=c(0,125),ylim=c(0,100), type = 'l')
-    plot(drugeff(2400,input$initconc,0.693,input$halflife,1000*input$killrate,input$ce50,input$h),xlim=c(0,125),ylim=c(0,100), type = 'l', ylab="Drug effect (1000*log10)", col="blue")
-    #plot(drugeff(2400,input$initconc,0.693,input$halflife,input$killrate,input$ce50,input$h),xlim=c(0,245),ylim=c(0,10), type = 'l')
+    #plot(drugeff(2400,input$initconc,0.693,input$halflife,killrate_R(),input$ce50,input$h),xlim=c(0,125),ylim=c(0,100), type = 'l')
+    plot(drugeff(2400,input$initconc,0.693,input$halflife,1000*killrate_R(),input$ce50,input$h),xlim=c(0,125),ylim=c(0,100), type = 'l', ylab="Drug effect (1000*log10)", col="blue")
+    #plot(drugeff(2400,input$initconc,0.693,input$halflife,killrate_R(),input$ce50,input$h),xlim=c(0,245),ylim=c(0,10), type = 'l')
     par(new=TRUE)
-    plot(drugeff(2400,input$initconc_2,0.693,input$halflife_2,1000*input$killrate_2,input$ce50_2,input$h), axes=FALSE, xlab="", ylab="", xlim=c(0,125),ylim=c(0,100), type = 'l',  col="red")
+    plot(drugeff(2400,input$initconc_2,0.693,input$halflife_2,1000*killrate_2_R(),input$ce50_2,input$h), axes=FALSE, xlab="", ylab="", xlim=c(0,125),ylim=c(0,100), type = 'l',  col="red")
     #ylab="Drug B effect (1000*log10)",
   })
   
--- within-host-shiny-malaria/ui.R
+++ within-host-shiny-malaria/ui.R
@@ -6,19 +6,22 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      sliderInput("initn", "Initial total parasites: ", min = 10, max=1000, value=10),
-      sliderInput("mu", "Mean of initial age distribution: ", min = 1, max=48, value = 1),
-      sliderInput("sig", "SD of initial age distribution: ", min = 1, max=48, value = 1),
-      sliderInput("pmf", "Parasite Multiplication factor: ", min = 1, max=30, value=1),
-      sliderInput("h", "h: ", min = 1, max = 10, value =1, step =1),
-      sliderInput("initconc", "Initial drug A concentration: ", min = 25, max=150, value = 25, step = 5 ),
-      sliderInput("halflife", "Drug A halflife: ", min = 5, max=240, value=5, step = 1),
-      sliderInput("killrate", "Kill rate, drug A: ", min = .001, max = 1, value = .05, step = .05),
-      sliderInput("ce50", "CE50, drug A: ", min = 10, max = 100, value = 10, step = 5),
+      #sliderInput("initn", "Initial total parasites: ", min = 1000, max=10^10, value=100),
+      sliderInput("initn", "Initial total parasites (10^x): ", min = 3, max=10, value=8.93, step=.01),
+      sliderInput("mu", "Mean of initial age distribution: ", min = 1, max=48, value = 28),
+      sliderInput("sig", "SD of initial age distribution: ", min = 1, max=48, value = 7),
+      sliderInput("pmf", "Parasite Multiplication factor: ", min = 8, max=10, value=1),
+      sliderInput("h", "h: ", min = 1, max = 10, value =4, step =1),
+      sliderInput("initconc", "Initial drug A concentration: ", min = 25, max=150, value = 72, step = 5 ),
+      sliderInput("halflife", "Drug A halflife: ", min = 5, max=240, value=54, step = 1),
+      sliderInput("killrate", "Kill rate, drug A: ", min = .05, max = .4, value = .2, step = .05),
+      sliderInput("sen", "Sensitivity, drug A: ", min = .00, max = 1.00, value = 1, step = .01),
+      sliderInput("ce50", "CE50, drug A: ", min = 10, max = 100, value = 15, step = 5),
       
       sliderInput("initconc_2", "Initial drug B concentration: ", min = 25, max=150, value = 50, step = 5 ),
-      sliderInput("halflife_2", "Drug B halflife: ", min = 5, max=240, value=20, step = 1),
-      sliderInput("killrate_2", "Kill rate, drug B: ", min = .001, max = 1, value = .001, step = .05),
+      sliderInput("halflife_2", "Drug B halflife: ", min = 5, max=400, value=324, step = 1),
+      sliderInput("killrate_2", "Kill rate, drug B: ", min = .05, max = .4, value = .1, step = .05),
+      sliderInput("sen_2", "Sensitivity, drug B: ", min = .00, max = 1.00, value = 1, step = .01),
       sliderInput("ce50_2", "CE50, drug B: ", min = 10, max = 100, value = 30, step = 5)
     ),
     mainPanel(
@@ -28,18 +31,4 @@ shinyUI(fluidPage(
   )
 ))
 
-
-##initn=slider(10,1000),
-##mu=slider(1,48),
-##sig=slider(1,48),
-##pmf=slider(1,30),
-# k0=slider(0,1,step = 0.01),
-# a=slider(0.0001,0.01,step = 0.001),
-# tpar=slider(1000,100000,step = 10),
-# delay=slider(0,4,step=1),
-##initconc=slider(25,150,step=5),
-##halflife=slider(5,240,step=1),
-##killrate=slider(0.001,1,step=0.05),
-##ce50=slider(10,100,step=5),
-##h=slider(1,10,step=1),
 
