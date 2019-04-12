560eee6b7ce0c9f4592a7fc54c0a5eb04e123ef7
SaiMaci9 <theinthantun.sai@gmail.com>
2019-04-12 13:00:22 +0700

    dha pip only, separate h

--- DHA-PIP-only/ui.R
+++ DHA-PIP-only/ui.R
@@ -9,28 +9,29 @@ shinyUI(fluidPage(
   fluidRow(
       #sliderInput("initn", "Initial total parasites: ", min = 1000, max=10^10, value=100),
     column(3,
-      sliderInput("initn", "Log10 of Initial total parasites: ", min = 3, max=14, value=8.93, step=.01),
+      sliderInput("initn", "Log10 of initial total parasites: ", min = 3, max=14, value=8.93, step=.01),
       sliderInput("mu", "Mean of initial age distribution: ", min = 1, max=48, value = 28),
       sliderInput("sig", "SD of initial age distribution: ", min = 1, max=48, value = 7),
-      sliderInput("pmf", "Parasite Multiplication factor: ", min = 8, max=10, value=8),
-      sliderInput("h", "h: ", min = 1, max = 10, value =4, step =1)
+      sliderInput("pmf", "Parasite Multiplication factor: ", min = 8, max=10, value=8)
     ),
       
-    column(3,
-           strong("DHA"),
+    column(4,
+           strong("DHA pharmacodynamics"),
        # sliderInput("initconc", "Initial drug A concentration: ", min = 25, max=150, value = 72, step = 5 ),
        # sliderInput("halflife", "Drug A halflife: ", min = 5, max=240, value=16, step = 1), #value=54, step = 1),
       sliderInput("killrate", "Kill rate: ", min = 0, max = .4, value = .2, step = .01),
       sliderInput("sen", "Sensitivity: ", min = .00, max = 1.00, value = 1, step = .01),
-      sliderInput("ce50", "CE50: ", min = 10, max = 100, value = 15, step = 5)
+      sliderInput("ce50", "CE50: ", min = 10, max = 100, value = 15, step = 5),
+      sliderInput("h", "h: ", min = 1, max = 10, value =4, step =1)
       ),
-      column(3, strong("Piperaquine"),
+      column(4, strong("Piperaquine pharmacodynamics"),
       #drug2
       # sliderInput("initconc_2", "Initial drug B concentration: ", min = 25, max=150, value = 50, step = 5 ),
       # sliderInput("halflife_2", "Drug B halflife: ", min = 5, max=400, value=324, step = 1),
       sliderInput("killrate_2", "Kill rate: ", min = 0, max = .4, value = .1, step = .01),
       sliderInput("sen_2", "Sensitivity: ", min = .00, max = 1.00, value = 1, step = .01),
-      sliderInput("ce50_2", "CE50: ", min = 10, max = 100, value = 30, step = 5)
+      sliderInput("ce50_2", "CE50: ", min = 10, max = 100, value = 30, step = 5),
+      sliderInput("h_2", "h: ", min = 1, max = 10, value =4, step =1)
       )
     # ,
     #   column(3,
