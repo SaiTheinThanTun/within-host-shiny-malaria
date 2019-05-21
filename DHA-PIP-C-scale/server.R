295902c17e15fdd9a1b5554e88630fc5a2d5f846
SaiMaci9 <theinthantun.sai@gmail.com>
2019-05-21 15:01:44 +0700

    utilityFun DHApip and 7 RDS

--- DHA-PIP-C-scale/server.R
+++ DHA-PIP-C-scale/server.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(ggplot2)
 
 #utility functions go here
 source("UtilityFunctions.R", local = TRUE)
@@ -11,7 +12,6 @@ shinyServer(function(input, output, session) {
   killrate_R <- reactive(input$killrate*input$sen) #killrate adjusted by sensitivity
   killrate_2_R <- reactive(input$killrate_2*input$sen_2)
   killrate_3_R <- reactive(input$killrate_3*input$sen_3)
-  ThirdDrugConc_R <- reactive(ThirdDrug(input$initconc_3, input$scaling))
   
   #TODO
   #MIC for 3rd drug hasn't be done as changes will still come to MIC #TODO
@@ -95,14 +95,27 @@ shinyServer(function(input, output, session) {
   #new plots for DHApip calibrated model
   output$paraPlot_DHApip <- renderPlot({
     
-    parasiteDensity_DHApip <- reactive(NJWIm_DHApip_C(initn_R(),48,input$mu,input$sig,input$pmf,k0,a,tpar,delay,2400,
+    parasiteDensity_DHApip <- reactive(NJWIm_DHApip(initn_R(),48,input$mu,input$sig,input$pmf,k0,a,tpar,delay,2400,
                                            killrate_R(),ce50_R(),input$h, 
-                                           killrate_2_R(),ce50_2_R(), input$h_2,
-                                           input$initconc_3,0.693,input$halflife_3,
-                                           killrate_3_R(),input$ce50_3,input$h_3))
+                                           killrate_2_R(),ce50_2_R(), input$h_2))
+    #output
+    #1. time, 2. log10, 3. normal, 4. gam, 5. infect
+    mycol <- rgb(0, 0, 255, max = 255, alpha = 100, names = "blue50")
     
-    plot(x=parasiteDensity_DHApip()[,1],y=parasiteDensity_DHApip()[,2], xlab="Time (hours)", ylab="Parasite density (log10)",xlim=c(0,1200),ylim=c(0,10), type = 'l')
-    text(x = 520, y=7, paste("Log-Sum of observable parasites: ", round(log10(sum(parasiteDensity_DHApip()[,3]))))) #paste("Log-Sum of observable parasites: ", sum(round(parasiteDensity_DHApip()[,2]))))
+    plot(x=parasiteDensity_DHApip()[,1],y=parasiteDensity_DHApip()[,2], xlab="Time (hours)", ylab="Parasite density (log10)",xlim=c(0,1200),ylim=c(0,max(c(8,parasiteDensity_DHApip()[,2]))), type = 'l')
+    lines(x=parasiteDensity_DHApip()[,1],y=parasiteDensity_DHApip()[,4], lty="dotdash")
+    auxillaryParasiteDensity <- parasiteDensity_DHApip()[,3]
+    auxillaryParasiteDensity[auxillaryParasiteDensity==0] <- 1
+    text(x = 520, y=7, paste("Log-Sum of observable parasites: ", round(sum(log10(auxillaryParasiteDensity)),3)))
+    legend(650, 4.5, legend = c("Total countable parasites", "Gametocytes"), lty = c(1,4))
+    par(new=TRUE)
+    plot(x=parasiteDensity_DHApip()[,1],y=parasiteDensity_DHApip()[,5], type='l',col='blue', axes=FALSE, xlab="", ylab = "", ylim=c(0,1.2))
+    #barplot(parasiteDensity_DHApip()[,5], axes=FALSE, col="red", border="red")
+    polygon(x=parasiteDensity_DHApip()[,1],y=c(parasiteDensity_DHApip()[,5][-nrow(parasiteDensity_DHApip())],0), col=mycol)
+    axis(4, at=c(0,1), col="blue",col.axis="blue",las=1)
+    mtext("Probability of infectiousness",side=4,col="blue")
+    
+    #text(x = 520, y=7, paste("Log-Sum of observable parasites: ", round(log10(sum(parasiteDensity_DHApip()[,3])),3))) #paste("Log-Sum of observable parasites: ", sum(round(parasiteDensity_DHApip()[,2]))))
     
     #TODO
     # abline(v=TrueMIC(whereIsMIC_D1_R()), col="blue")
@@ -118,34 +131,50 @@ shinyServer(function(input, output, session) {
     drugConc_B <- PIPconcentration #reactive(drugf(2400,input$initconc_2,0.693,input$halflife_2,killrate_2_R(),ce50_2_R(),input$h))
     drugConc_B[,2] <- log10(drugConc_B[,2])
     drugConc_B[,2][is.infinite(drugConc_B[,2])] <- 0
-    drugConc_C <- reactive(drugf(2400,input$initconc_3,0.693,input$halflife_3,killrate_3_R(),input$ce50_3,input$h_3))
+    #drugConc_C <- reactive(drugf(2400,input$initconc_3,0.693,input$halflife_3,killrate_3_R(),input$ce50_3,input$h))
     
     #par(mar=c(5, 4, 4, 6) + 0.1)
-    plot(drugConc_A,xlab="Time (hours)", ylab="Drug concentration (log10)",xlim=c(0,400),ylim=c(0,max(c(max(drugConc_A[,2]),max(drugConc_B[,2]),max(drugConc_C()[,2])))), type = 'l', col="blue")
-    #plot(drugConc_A,xlab="Time (hours)", ylab="Drug concentration (log10)",xlim=c(0,400),ylim=c(0,max(c(max(drugConc_A[,2]),max(drugConc_B[,2])))), type = 'l', col="blue")
+    #plot(drugConc_A,xlab="Time (hours)", ylab="Drug concentration (log10)",xlim=c(0,400),ylim=c(0,max(c(max(drugConc_A[,2]),max(drugConc_B[,2]),max(drugConc_C()[,2])))), type = 'l', col="blue")
+    plot(drugConc_A,xlab="Time (hours)", ylab="Drug concentration (log10)",xlim=c(0,400),ylim=c(0,max(c(max(drugConc_A[,2]),max(drugConc_B[,2])))), type = 'l', col="blue")
     par(new=TRUE)
-    plot(drugConc_B, axes=FALSE,xlab="", ylab="",xlim=c(0,400),ylim=c(0,max(c(max(drugConc_A[,2]),max(drugConc_B[,2]),max(drugConc_C()[,2])))), type = 'l', col="red")
-    #plot(drugConc_B, axes=FALSE,xlab="", ylab="",xlim=c(0,400),ylim=c(0,max(c(max(drugConc_A[,2]),max(drugConc_B[,2])))), type = 'l', col="red")
+    #plot(drugConc_B, axes=FALSE,xlab="", ylab="",xlim=c(0,400),ylim=c(0,max(c(max(drugConc_A[,2]),max(drugConc_B[,2]),max(drugConc_C()[,2])))), type = 'l', col="red")
+    plot(drugConc_B, axes=FALSE,xlab="", ylab="",xlim=c(0,400),ylim=c(0,max(c(max(drugConc_A[,2]),max(drugConc_B[,2])))), type = 'l', col="red")
     par(new=TRUE)
-    plot(drugConc_C(), axes=FALSE,xlab="", ylab="",xlim=c(0,400),ylim=c(0,max(c(max(drugConc_A[,2]),max(drugConc_B[,2]),max(drugConc_C()[,2])))), type = 'l', col="black")
-    legend(150, 1, legend = c("DHA", "Piperaquine", "Drug C"), col = c("blue", "red", "black"), lty = 1)
-    #legend(150, 1, legend = c("DHA", "Piperaquine"), col = c("blue", "red"), lty = 1)
+    #plot(drugConc_C(), axes=FALSE,xlab="", ylab="",xlim=c(0,400),ylim=c(0,max(c(max(drugConc_A[,2]),max(drugConc_B[,2]),max(drugConc_C()[,2])))), type = 'l', col="black")
+    #legend(150, 1, legend = c("DHA", "Piperaquine", "Drug C"), col = c("blue", "red", "black"), lty = 1)
+    legend(150, 1, legend = c("DHA", "Piperaquine"), col = c("blue", "red"), lty = 1)
   })
   
   output$drugeffPlot_DHApip <- renderPlot({
     drugEff_A <- reactive(SpecificDrugEffect(2400,"DHA",killrate_R(),ce50_R(),input$h))
     drugEff_B <- reactive(SpecificDrugEffect(2400,"pip",killrate_2_R(),ce50_2_R(),input$h_2))
-    drugEff_C <- reactive(drugeff(2400,input$initconc_3,0.693,input$halflife_3,killrate_3_R(),input$ce50_3,input$h_3))
-    #plot(drugEff_A(), xlim=c(0,400),ylim=c(0,max(c(max(drugEff_A()[,2]),max(drugEff_B()[,2])))), type = 'l', ylab="Drug effect", col="blue")
-    plot(drugEff_A(), xlim=c(0,400),ylim=c(0,max(c(max(drugEff_A()[,2]),max(drugEff_B()[,2]),max(drugEff_C()[,2])))), type = 'l', xlab="Time (hours)", ylab="Drug effect", col="blue")
+    #drugEff_C <- reactive(drugeff(2400,input$initconc_3,0.693,input$halflife_3,killrate_3_R(),input$ce50_3,input$h))
+    plot(drugEff_A(), xlim=c(0,400),ylim=c(0,max(c(max(drugEff_A()[,2]),max(drugEff_B()[,2])))), type = 'l', ylab="Drug effect", col="blue")
+    #plot(drugEff_A(), xlim=c(0,400),ylim=c(0,max(c(max(drugEff_A()[,2]),max(drugEff_B()[,2]),max(drugEff_C()[,2])))), type = 'l', xlab="Time (hours)", ylab="Drug effect", col="blue")
     par(new=TRUE)
-    #plot(drugEff_B(), axes=FALSE, xlab="", ylab="", xlim=c(0,400),ylim=c(0,max(c(max(drugEff_A()[,2]),max(drugEff_B()[,2])))), type = 'l',  col="red")
-    plot(drugEff_B(), axes=FALSE, xlab="", ylab="", xlim=c(0,400),ylim=c(0,max(c(max(drugEff_A()[,2]),max(drugEff_B()[,2]),max(drugEff_C()[,2])))), type = 'l',  col="red")
-    par(new=TRUE)
-    plot(drugEff_C(), axes=FALSE, xlab="", ylab="", xlim=c(0,400),ylim=c(0,max(c(max(drugEff_A()[,2]),max(drugEff_B()[,2]),max(drugEff_C()[,2])))), type = 'l',  col="black")
-    #legend(150, .05, legend = c("DHA", "Piperaquine"), col = c("blue", "red"), lty = 1)
-     legend(150, .05, legend = c("DHA", "Piperaquine", "Drug C"), col = c("blue", "red", "black"), lty = 1)
+    plot(drugEff_B(), axes=FALSE, xlab="", ylab="", xlim=c(0,400),ylim=c(0,max(c(max(drugEff_A()[,2]),max(drugEff_B()[,2])))), type = 'l',  col="red")
+    #plot(drugEff_B(), axes=FALSE, xlab="", ylab="", xlim=c(0,400),ylim=c(0,max(c(max(drugEff_A()[,2]),max(drugEff_B()[,2]),max(drugEff_C()[,2])))), type = 'l',  col="red")
+    #par(new=TRUE)
+    #plot(drugEff_C(), axes=FALSE, xlab="", ylab="", xlim=c(0,400),ylim=c(0,max(c(max(drugEff_A()[,2]),max(drugEff_B()[,2]),max(drugEff_C()[,2])))), type = 'l',  col="black")
+    legend(150, .05, legend = c("DHA", "Piperaquine"), col = c("blue", "red"), lty = 1)
+    # legend(150, .05, legend = c("DHA", "Piperaquine", "Drug C"), col = c("blue", "red", "black"), lty = 1)
   })
   
+  output$killRate48 <- renderPlot({
+    paraDen <- reactive(NJWIm_DHApip(initn_R(),48,input$mu,input$sig,input$pmf,k0,a,tpar,delay,2400,
+                                                    killrate_R(),ce50_R(),input$h,
+                                                    killrate_2_R(),ce50_2_R(), input$h_2))
+    #output
+    #1. time, 2. log10, 3. normal, 4. gam, 5. infect
+    every48 <- seq(from=1,to=2400, by=48)
+    paraDen48 <- reactive(paraDen()[every48,3])
+    killRate48 <- NA
+    for(i in 1:(length(paraDen48())-1)){
+      killRate48[i] <- paraDen48()[i]*8-paraDen48()[i+1]
+    }
 
+    killRate48[which(killRate48==0)] <- 1 #to prevent infinity values from log10
+
+    barplot(log10(killRate48)[1:25], main = "Kill rate \n10*(# parasites at a timepoint)-(# parasites after 48 hours)", xlab="Time @48 hours interval", ylab = "Log10(parasite difference between 48 hr)", ylim=c(0,max(log10(killRate48))))
+  })
 })
--- DHA-PIP-C-scale/ui.R
+++ DHA-PIP-C-scale/ui.R
@@ -2,29 +2,30 @@ library(shiny)
 
 shinyUI(fluidPage(
   
-  titlePanel("Within Host Drug Action: DHA, piperaquine + drug C"),
+  titlePanel("Within Host Drug Action: DHA & piperaquine only"),
   
   # sidebarLayout(
   #   sidebarPanel(
   fluidRow(
       #sliderInput("initn", "Initial total parasites: ", min = 1000, max=10^10, value=100),
     column(3,
-      sliderInput("initn", "Log10 of Initial total parasites: ", min = 3, max=14, value=8.93, step=.01),
+      sliderInput("initn", "Log10 of initial total parasites: ", min = 3, max=14, value=8.93, step=.01),
       sliderInput("mu", "Mean of initial age distribution: ", min = 1, max=48, value = 28),
       sliderInput("sig", "SD of initial age distribution: ", min = 1, max=48, value = 7),
       sliderInput("pmf", "Parasite Multiplication factor: ", min = 8, max=10, value=8)
     ),
       
-    column(3, strong("DHA pharmacodynamics"),
-      # sliderInput("initconc", "Initial drug A concentration: ", min = 25, max=150, value = 72, step = 5 ),
-      # sliderInput("halflife", "Drug A halflife: ", min = 5, max=240, value=16, step = 1), #value=54, step = 1),
+    column(4,
+           strong("DHA pharmacodynamics"),
+       # sliderInput("initconc", "Initial drug A concentration: ", min = 25, max=150, value = 72, step = 5 ),
+       # sliderInput("halflife", "Drug A halflife: ", min = 5, max=240, value=16, step = 1), #value=54, step = 1),
       sliderInput("killrate", "Kill rate: ", min = 0, max = .4, value = .2, step = .01),
       sliderInput("sen", "Sensitivity: ", min = .00, max = 1.00, value = 1, step = .01),
       #sliderInput("ce50", "CE50: ", min = 10, max = 100, value = 15, step = 5),
       sliderInput("ce50", "Log10 of CE50: ", min = .5, max = 3, value = .4, step = .1),
       sliderInput("h", "h: ", min = 1, max = 10, value =4, step =1)
       ),
-      column(3,  strong("Piperaquine pharmacodynamics"),
+      column(4, strong("Piperaquine pharmacodynamics"),
       #drug2
       # sliderInput("initconc_2", "Initial drug B concentration: ", min = 25, max=150, value = 50, step = 5 ),
       # sliderInput("halflife_2", "Drug B halflife: ", min = 5, max=400, value=324, step = 1),
@@ -33,32 +34,33 @@ shinyUI(fluidPage(
       #sliderInput("ce50_2", "CE50: ", min = 10, max = 100, value = 30, step = 5),
       sliderInput("ce50_2", "Log10 of CE50: ", min = .5, max = 3, value = .6, step = .1),
       sliderInput("h_2", "h: ", min = 1, max = 10, value =4, step =1)
-      ),
-      column(3, strong("Drug C"),
-      #drug3
-      sliderInput("initconc_3", "Initial drug concentration: ", min = 25, max=150, value = 60, step = 5 ),
-      sliderInput("halflife_3", "Halflife: ", min = 5, max=400, value=24, step = 1),
-      sliderInput("killrate_3", "Kill rate: ", min = 0, max = .4, value = .15, step = .01),
-      sliderInput("sen_3", "Sensitivity: ", min = .00, max = 1.00, value = .8, step = .01),
-      sliderInput("ce50_3", "CE50: ", min = 10, max = 100, value = 25, step = 5),
-      sliderInput("h_3", "h: ", min = 1, max = 10, value =4, step =1)
       )
+    # ,
+    #   column(3,
+    #   #drug3
+    #   sliderInput("initconc_3", "Initial drug C concentration: ", min = 25, max=150, value = 60, step = 5 ),
+    #   sliderInput("halflife_3", "Drug C halflife: ", min = 5, max=400, value=24, step = 1),
+    #   sliderInput("killrate_3", "Kill rate, drug C: ", min = 0, max = .4, value = .15, step = .01),
+    #   sliderInput("sen_3", "Sensitivity, drug C: ", min = .00, max = 1.00, value = .8, step = .01),
+    #   sliderInput("ce50_3", "CE50, drug C: ", min = 10, max = 100, value = 25, step = 5)
+    #   )
      ),
   fluidRow(p("Sensitivity to drug, toggle:"),
-           actionButton("aSen", "A:100%, B:50%"),
-           actionButton("bSen", "A:50%, B:100%"))
-  # ,
+           actionButton("aSen", "DHA:100%, pip:50%"),
+           actionButton("bSen", "DHA:50%, pip:100%")),
   # fluidRow(
   #   # mainPanel(
   #     column(4,plotOutput("paraPlot")),
   #     column(4,plotOutput("combinedPlot")),
   #     column(4,plotOutput("drugeffPlot"))
-  #   )
-  ,
+  #   ),
   fluidRow(
     column(4,plotOutput("paraPlot_DHApip")),
     column(4,plotOutput("DHA_PIP_Plot")),
     column(4,plotOutput("drugeffPlot_DHApip"))
+  ),
+  fluidRow(
+    column(4,plotOutput(("killRate48")))
   )
   )
 )
